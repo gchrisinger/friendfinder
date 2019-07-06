f319a42dcca1e08049433507ff4a808eeaa8d330
Georgia <gchrisinger@gmail.com>
2019-07-06 07:57:19 -0700

    added routing and other folders

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var app = express ();
 
-var PORT = process.env.PORT || 9335;
+// var PORT = process.env.PORT || 9335;
+var PORT = 9335;
 
 app. get('/',function(req,res){
-    res.send('hello there')
+    res.send('hello there friend finder')
 })
 app.listen(PORT, function() {
     console.log("app listening on port" + PORT);
 })
-
