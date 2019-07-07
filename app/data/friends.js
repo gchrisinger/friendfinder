e61097d27927d5604cca6e59ed9f49956ea7f515
Georgia <gchrisinger@gmail.com>
2019-07-06 21:19:16 -0700

    not sure

---
+++ app/data/friends.js
@@ -0,0 +1,85 @@
+var friendArray = [
+    {
+      "name": " Jon Snow",
+      "photo": "https://cdn1.thr.com/sites/default/files/imagecache/scale_crop_768_433/2017/08/helen_sloan_-_hbo_photo_43_-_h_2017.jpg",
+      "scores": [
+        5,
+        1,
+        4,
+        1,
+        3,
+        1,
+        2,
+        4,
+        4,
+        1
+      ]
+    },
+    {
+      "name": "Tyrion Lannister",
+      "photo": "https://images.immediate.co.uk/volatile/sites/3/2017/10/game-of-thrones-season-5-7c44358.jpg?quality=90&crop=192px,0px,1079px,719px&resize=620,413",
+      "scores": [
+        5,
+        1,
+        3,
+        1,
+        5,
+        1,
+        5,
+        5,
+        2,
+        1
+      ]
+    },
+    {
+      "name": "Daenerys Targaryen",
+      "photo": "https://fsmedia.imgix.net/c1/74/eb/ec/7ef9/4cfe/a39c/92ab48cf8dcc/ep50-ss01-1920jpg.jpeg?rect=0%2C0%2C1920%2C960&dpr=2&auto=format%2Ccompress&w=650",
+      "scores": [
+        1,
+        1,
+        4,
+        5,
+        4,
+        1,
+        3,
+        3,
+        2,
+        1
+      ]
+    },
+    {
+      "name": "Cersei Lannister",
+      "photo": "https://images.immediate.co.uk/volatile/sites/3/2017/08/promo324259790.png?quality=90&resize=620,413",
+      "scores": [
+        5,
+        2,
+        4,
+        4,
+        1,
+        3,
+        2,
+        2,
+        4,
+        1
+      ]
+    },
+    {
+      "name": "Arya Stark",
+      "photo": "http://images6.fanpop.com/image/photos/39600000/arya-stark-house-stark-39637640-1280-1019.jpg",
+      "scores": [
+        1,
+        1,
+        4,
+        5,
+        2,
+        1,
+        5,
+        4,
+        4,
+        3
+      ]
+    }
+  ];
+  
+  // Note how we export the array. This makes it accessible to other files using require.
+  module.exports = friendArray;
