d4f289a85bc4391f8eb70db34f66cbbffc317f51
Georgia <gchrisinger@gmail.com>
2019-07-07 10:57:41 -0700

    added things

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -39,7 +39,7 @@ module.exports = function(app) {
 
   // ---------------------------------------------------------------------------
 //   // I added this below code so you could clear out the table while working with the functionality.
-//   // Don"t worry about it!
+// //   // Don"t worry about it!
 
 //   app.post("/api/clear", function(req, res) {
 //     // Empty out the arrays of data
